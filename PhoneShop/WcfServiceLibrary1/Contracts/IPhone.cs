fe48fe7cf1eb8b1f24d218a58f5935e19d7db638
boyarchuk <boyarchuk@A34COMP6>
2018-10-31 16:13:01 +0200

    Create Interfaces

---
+++ PhoneShop/WcfServiceLibrary1/Contracts/IPhone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceLibrary1.Contracts
+{
+    [ServiceContract]
+    public  interface IPhone
+    {
+
+    }
+}
