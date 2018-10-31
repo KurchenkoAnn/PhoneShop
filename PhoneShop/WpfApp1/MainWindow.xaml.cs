90b05914fec9a80c3e51514e4cafce95d80221ce
Ann <user@A34COMP1>
2018-10-31 17:52:20 +0200

    Start2

--- PhoneShop/WpfApp1/MainWindow.xaml.cs
+++ PhoneShop/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,30 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Models;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        List<UserUI> u = new List<UserUI>();
+        List<OrderUI> o = new List<OrderUI>();
+        List<PhoneUI> p = new List<PhoneUI>();
+
         public MainWindow()
         {
             InitializeComponent();
+            u = new List<UserUI>() { new UserUI { Name = "Vasyan", Email = "vasyanVas" } };
+            o = new List<OrderUI>() { new OrderUI {} };
+            p = new List<PhoneUI>() { new PhoneUI { Model="Samsung J3 2016",Producer="Samsung",Price=2700} };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
---
+++ PhoneShop/WpfApp1/Models/OrderUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public class OrderUI
+    {
+        public int ID { get; set; }
+        public UserUI Customer{get;set;}
+        public PhoneUI Item { get; set; }
+        public DateTime GetOrder { get; set; }
+    }
+}
